909d9bb18242fa08e08ab179c99bc9fcfeb731b3
Norbert Fejer <norbertfejer@protonmail.com>
2020-11-29 11:00:54 +0200

    news feed added

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from firebase_admin import credentials, firestore, initialize_app
 from flask_cors import CORS, cross_origin
 
 import uuid
-
+from newsapi import NewsApiClient
 import sys
 
 # Initialize Flask app
@@ -352,6 +352,26 @@ def getCabinetBySymptons():
     except Exception as e:
         return jsonify({"msg": e}), 500
 
+
+@app.route('/getNewsFeed', methods=['GET'])
+def getNewsFeed():
+
+    try:
+        newsapi = NewsApiClient(api_key='1c9365fa29e64db9a6e27f1cbd18ea88')
+        page_num = request.args.get('page')
+        page_size_num = request.args.get('pageSize')
+
+        if page_num and page_size_num:
+            top_headlines = newsapi.get_top_headlines(q='vírus',
+                                                        country='hu',
+                                                        page=int(page_num),
+                                                        page_size=int(page_size_num))
+
+            return jsonify(top_headlines), 200
+        else:
+            return jsonify({"success": False}), 405
+    except Exception as e:
+        return jsonify({"msg": "An error occured!"}), 500
 
 
 
